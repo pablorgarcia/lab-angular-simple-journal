bd03aab0342e7a4402aa76cb82691e22d1acd000
Pablo García <heypablete@gmail.com>
2018-05-03 13:23:17 +0200

    iteracion2 done

---
+++ starter-code/client/src/app/entry-list/entry-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { JournalServiceService } from '../journal-service.service';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css'],
+  providers: [JournalServiceService]
+})
+
+export class EntryListComponent implements OnInit {
+
+  entries: Array<string>;
+
+  constructor( private entry: JournalServiceService) { }
+
+  ngOnInit() {
+    this.entry.getList().subscribe(
+      (entries) => { this.entries = entries; }
+    );
+  }
+
+}
---
+++ starter-code/client/src/app/journal-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class JournalServiceService {
+
+  BASE_URL: string = 'http://localhost:3000';
+
+  constructor(private http: Http) {}
+
+  getList() {
+    return this.http.get(`${this.BASE_URL}/api/journal-entries`)
+    .map( (res) => res.json() );
+  }
+
+}
